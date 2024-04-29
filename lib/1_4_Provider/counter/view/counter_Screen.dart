27ea849d650eda670a68120ec0f85b22900ac8c9
~JAY GOGA~ <dkpatel3116@gmail.com>
2024-04-29 22:28:08 +0530

    29-4-24

---
+++ lib/1_4_Provider/counter/view/counter_Screen.dart
@@ -0,0 +1,37 @@
+import 'package:adv_flutter_ch1/1_4_Provider/counter/provider/counter_provider.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class CounterScreen extends StatelessWidget {
+  const CounterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xff4F6F52),
+        title: Text("Counter App",style: TextStyle(
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+          fontSize: 25
+
+        ),),
+        centerTitle: true,
+      ),
+      body: Center(child: Text( Provider.of<CounterProvider>(context, listen: true).count.toString(),style: TextStyle(
+        fontSize: 30,
+        fontWeight: FontWeight.bold
+      ),),),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Color(0xff4F6F52),
+        onPressed: () {
+          Provider.of<CounterProvider>(context, listen: false).add();
+          },
+        child: Icon(Icons.add),
+
+
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:adv_flutter_ch1/1_4_Provider/counter/provider/counter_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import ' Switch Dark Theme to Light Theme/Theme_Screen.dart';
+import '1_4_Provider/counter/view/counter_Screen.dart';
 import 'Stepper/Stepper_Screen.dart';
 
 void main() {
-  runApp( MyApp());
+  runApp( MultiProvider(providers: [
+
+    ChangeNotifierProvider(create: (context) => CounterProvider(),),
+  ],child: MyApp(),));
 }
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
@@ -24,11 +30,12 @@ class _MyAppState extends State<MyApp> {
           style: ButtonStyle(backgroundColor: MaterialStatePropertyAll(Colors.red))
         )
       ),
-      initialRoute: '/Stepper',
+      initialRoute: '/CounterScreen',
       routes: {
 
         '/Stepper':(context) => StepperApp(),
         '/Light/dark':(context) => ThemeScreen(),
+        '/CounterScreen':(context) => CounterScreen(),
 
       },
     );
